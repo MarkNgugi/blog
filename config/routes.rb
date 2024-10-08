20be3cdf003698a8f7d2d90b1a58669d924b92e7
mark <mmarkmarkm0@gmail.com>
2024-10-08 21:14:00 +0300

    generated a controller:

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-end
+
+  root "articles#index"
+
+  get "/articles", to: "articles#index"
+   
+end 
